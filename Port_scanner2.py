10edc55dcdd4f801c4b73dd4db226b9d9695edd6
Manish Sarkar <sarkarmrigank28@gmail.com>
2025-07-30 23:15:47 +0530

    Update it with Progress bar

--- Port_scanner2.py
+++ Port_scanner2.py
@@ -1,6 +1,7 @@
 from scapy.all import IP, TCP, sr1
 import threading
 import click
+from tqdm import tqdm
 
 open_ports = []
 
@@ -9,12 +10,14 @@ def syn_scan(ip, port):
     resp = sr1(pkt, timeout = 1, verbose = 0)
     if resp and resp.haslayer(TCP):
         if resp[TCP].flags == 0x12:
-            click.secho(f"[+] Port {port} open !", fg = 'green')
+            tqdm.write(click.style(f"[+] Port {port} open !", fg = 'cyan', bold = True))
             open_ports.append(port)
 
 def Threaded_scan(ip, start_port = 1, end_port = 65535):
     threads_list = []
-    for port in range(start_port, end_port + 1):
+    port_range = range(start_port, end_port +1)
+
+    for port in tqdm(port_range, desc = "Scanning Ports : "):
         t = threading.Thread(target = syn_scan, args = (ip, port))
         threads_list.append(t)
         t.start()
@@ -30,5 +33,5 @@ if __name__ == "__main__":
     ip = input("Enter the IP of target : ").strip()
     click.secho(f"[*] Starting SYN scan on {ip}... ", fg = "blue")
 
-    Threaded_scan(ip, 1, 1000)
+    Threaded_scan(ip)
     click.secho(f"\nScan Complete. \nOpen ports : {open_ports}", fg = "yellow")
